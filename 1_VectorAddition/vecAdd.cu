f82a39d164c2189ae0c51c4c81364bf01126d1fd
Amirali <amiralimiri3100@gmail.com>
2024-05-17 17:23:24 +0330

    move error file to utils

--- 1_VectorAddition/vecAdd.cu
+++ 1_VectorAddition/vecAdd.cu
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "gpuerrors.h"
+#include "../utils/gpuerrors.h"
 
 //-----------------------------------------------------------------------------
 void fill(float* data, int size) {
--- 2_colorToGrayscaleConversion/RGB2GRAY.cu
+++ 2_colorToGrayscaleConversion/RGB2GRAY.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "gpuerrors.h"
+#include "../utils/gpuerrors.h"
 
 //-----------------------------------------------------------------------------
 
